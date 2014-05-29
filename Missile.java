5888bc8058753540e1bb45ca53721946e7052d95
unknown <10010732@D6MM4J1.SBSCHOOLS.ORG>
2014-05-29 13:26:23 -0400

    hey

---
+++ Missile.java
@@ -0,0 +1,94 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.awt.image.*;
+import java.applet.*;
+import java.util.ArrayList;
+
+public class Missile{
+
+	int dR = 10;
+	public static final int ENEMY = 0;
+	public static final int CHILD = 2;
+	public static final int MINE = 1;
+
+
+	Point start,end,current;
+	int r = 0;
+	int type;
+	double d;
+	Color c = Color.WHITE;
+	boolean dead=false;
+	boolean breaked=false;
+
+
+	public Missile(Point start, Point end, Color c, int type){
+
+		current = start;
+		this.start = start;
+		this.end = end;
+		this.c=c;
+		this.type = type;
+		d = start.distance(end);
+
+		if(type == ENEMY || type == CHILD){
+			dR = 2;
+		}
+
+	}
+
+		public Missile(Point start, Point end, int type){
+
+			current = start;
+			this.start = start;
+			this.end = end;
+			this.type = type;
+			d = start.distance(end);
+
+			if(type == ENEMY || type == CHILD){
+				dR = 2;
+			}
+
+	}
+
+	public static Point endPoint(){
+		return new Point((int)(Math.random()*MissileCommand.WIDTH),
+			(int)(Math.random()*(60)+(MissileCommand.HEIGHT-60)));
+	}
+
+	public static Missile newMissile(int side){
+		return new Missile(new Point((int)(Math.random()*MissileCommand.WIDTH),0),endPoint(),side);
+	}
+
+	public static Missile newMissile(Point start, Point end, int side){
+		return new Missile(start,end,side);
+	}
+
+	public void move(){
+
+		if(current.getX()>MissileCommand.WIDTH || current.getX()<0 || current.getY()<0 || current.getY()>MissileCommand.HEIGHT){
+			dead = true;
+		}
+		else{
+		r+=dR;
+
+		int x2 = (int)(start.getX()+((end.getX()-start.getX())*r)/d);
+		int y2 = (int)(start.getY()+((end.getY()-start.getY())*r)/d);
+		current = new Point(x2,y2);
+
+		if(y2>MissileCommand.HEIGHT/2)
+			breaked=true;
+
+		}
+
+	}
+
+	public void draw(Graphics g){
+		g.setColor(c);
+		g.drawLine((int)start.getX(),(int)start.getY(),(int)current.getX(),(int)current.getY());
+	}
+
+}
